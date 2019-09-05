a753b0971b1eb249081c9ca49af3405ee636faba
Allan Mercou <allan.mercou@icloud.com>
2019-09-05 16:28:51 +0200

    Remove empty useless ctor.

--- src/ShareY/Attributes/RequiresAuthentication.cs
+++ src/ShareY/Attributes/RequiresAuthentication.cs
@@ -5,9 +5,5 @@ namespace ShareY.Attributes
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
     public class RequiresAuthentication : Attribute
     {
-        public RequiresAuthentication()
-        {
-
-        }
     }
 }
